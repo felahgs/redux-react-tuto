a150eab8742da3f5864abd55bed11f48fc48f985
Felipe Garcia de Souza <f.souza@cinq.com.br>
2019-01-24 13:30:49 -0200

    add extra dispatch actions

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -3,6 +3,32 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        console.log("ADD", action)
+        return {
+            ...state,
+            counter: state.counter + action.value,
+        }
+    }
+    if (action.type === 'SUBTRACT') {
+        console.log("SUBTRACT", action)
+        return {
+            ...state,
+            counter: state.counter - action.value 
+        }
+    }
     return state;
 };
 
