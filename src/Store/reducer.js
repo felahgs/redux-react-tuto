7c57b45f70699058a5fe379c8e9e193238892c48
Felipe Garcia de Souza <f.souza@cinq.com.br>
2019-01-24 14:27:20 -0200

    Updating state immutably

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -1,35 +1,39 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results:[]
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            ...state,
-            counter: state.counter + 1
-        }
+    switch (action.type) {
+        case 'INCREMENT':
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.value,
+            }
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.value 
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+
+        default:
+            return state;
     }
-    if (action.type === 'DECREMENT') {
-        return {
-            ...state,
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        console.log("ADD", action)
-        return {
-            ...state,
-            counter: state.counter + action.value,
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        console.log("SUBTRACT", action)
-        return {
-            ...state,
-            counter: state.counter - action.value 
-        }
-    }
-    return state;
+    // return state;
 };
 
 export default reducer;
